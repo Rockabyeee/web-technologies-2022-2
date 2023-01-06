4fe2a4c33e44c9880e2b2851edd429e33a114977
Rockabyeee <serg.bondarenko109823@mail.ru>
2023-01-06 18:48:22 +0500

    Сделал задание 1

--- 1.js
+++ 1.js
@@ -1,1 +1,21 @@
-console.log('Hello');
+function pickPropArray(students, property){
+    let result = [];
+    for (let student of students) {
+        if (student.hasOwnProperty(property)) {
+            result.push(student[property])
+        }
+    }
+    return result;
+}
+
+const students = [
+    { name: 'Павел', age: 20 },
+    { name: 'Иван', age: 20 },
+    { name: 'Эдем', age: 20 },
+    { name: 'Денис', age: 20 },
+    { name: 'Виктория', age: 20 },
+    { age: 40 },
+];
+
+const result = pickPropArray(students, 'name');
+console.log(result);
